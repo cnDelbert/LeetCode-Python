48b83ca3e2f98e94d1b5af53f076143580f1f059
Delbert <code@delbert.me>
2015-11-02 23:23:25 +0800

    plus one

---
+++ Plus One.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def plusOne(self, digits):
+        """
+        :type digits: List[int]
+        :rtype: List[int]
+        """
+        return [int(i) for i in str(int(''.join(str(s) for s in digits)) + 1)]
